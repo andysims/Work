d76628f893ac0fcde4c9eef5c93673d97af94a4f
Andy Sims <contact@asims.dev>
2020-06-30 14:27:51 -0700

    Py Scripting

--- Eckersall/DataProcessing/py/exportCensusKMZs.py
+++ Eckersall/DataProcessing/py/exportCensusKMZs.py
@@ -1,5 +1,19 @@
-import os, csv, arcpy
-# import psycopg2, shutil
+import os
+import csv
+import arcpy
+from datetime import datetime
+# import psycopg2
+# import shutil
+
+
+# Get date and time
+def date_and_time():
+    now = datetime.now()
+    fmt_dt = now.strftime("%d/%m/%Y %H:%M:%S")
+    return fmt_dt
+
+
+start_time = date_and_time()
 
 # KMZ Export Location
 kmzDir = "C:/Directory/Location"
@@ -36,7 +50,7 @@ with open(fips_lookup_csv) as f:
 
 # Read fips lookup to create directory and set def query
 # 1 = fips, 2 = st, 4 = county no space (county_nospc), 5 = st_full
-
+print('Exporting KMZs...')
 for state in fips_lookup:
         mainDirName = kmzDir + "/" + state[5]
         if os.path.exists(mainDirName):
@@ -55,7 +69,7 @@ for fips_rec in fips_lookup:
         mainDirPath = kmzDir + "/" + st_full
         dirName = mainDirPath + "/" + st_county
 
-        print("Processing " + st_county)
+        # print("Processing " + st_county)
 
         if os.path.isdir(mainDirPath):
                 #if os.path.isdir(dirName):
@@ -63,21 +77,25 @@ for fips_rec in fips_lookup:
                 #        os.mkdir(dirName)
                 #else:
                 #        os.mkdir(dirName)
-		# change definition query
-		mxd = arcpy.mapping.MapDocument(workspace)
-		df = arcpy.mapping.ListDataFrames(mxd)[0]
-		cb_layer = arcpy.mapping.ListLayers(mxd)[2]
-		cb_layer.definitionQuery = "fips = '{}'".format(fips)
-		cbg_layer = arcpy.mapping.ListLayers(mxd)[1]
-		cbg_layer.definitionQuery = "fips = '{}'".format(fips)
-		ct_layer = arcpy.mapping.ListLayers(mxd)[0]
-		ct_layer.definitionQuery = "fips = '{}'".format(fips)
+        # change definition query
+        mxd = arcpy.mapping.MapDocument(workspace)
+        df = arcpy.mapping.ListDataFrames(mxd)[0]
+        cb_layer = arcpy.mapping.ListLayers(mxd)[2]
+        cb_layer.definitionQuery = "fips = '{}'".format(fips)
+        cbg_layer = arcpy.mapping.ListLayers(mxd)[1]
+        cbg_layer.definitionQuery = "fips = '{}'".format(fips)
+        ct_layer = arcpy.mapping.ListLayers(mxd)[0]
+        ct_layer.definitionQuery = "fips = '{}'".format(fips)
         # update map view
-		Extent = cbg_layer.getExtent(True)
-		df.extent = Extent
-		arcpy.RefreshActiveView()
-		mxd.saveACopy(kmzDir + "/blocksCurrent.mxd", "10.3")
-		filename = dirName + "/" + st_county + ".kmz"
-		#arcpy.MapToKML_conversion(mxd, "Layers", filename, "0", "NO_COMPOSITE", "VECTOR_TO_IMAGE", "DEFAULT", "1024", "96", "CLAMPED_TO_GROUND")
-		arcpy.MapToKML_conversion(in_map_document=kmzDir+"/blocksCurrent.mxd", data_frame="Layers", out_kmz_file= kmzDir + st_full + "/" + st + "_" + county + ".kmz", map_output_scale="0", is_composite="NO_COMPOSITE", is_vector_to_raster="VECTOR_TO_IMAGE", extent_to_export="DEFAULT", image_size="1024", dpi_of_client="96", ignore_zvalue="CLAMPED_TO_GROUND")
-		os.remove(kmzDir+"/blocksCurrent.mxd")
+        Extent = cbg_layer.getExtent(True)
+        df.extent = Extent
+        arcpy.RefreshActiveView()
+        mxd.saveACopy(kmzDir + "/blocksCurrent.mxd", "10.3")
+        filename = dirName + "/" + st_county + ".kmz"
+        #arcpy.MapToKML_conversion(mxd, "Layers", filename, "0", "NO_COMPOSITE", "VECTOR_TO_IMAGE", "DEFAULT", "1024", "96", "CLAMPED_TO_GROUND")
+        arcpy.MapToKML_conversion(in_map_document=kmzDir+"/blocksCurrent.mxd", data_frame="Layers", out_kmz_file= kmzDir + st_full + "/" + st + "_" + county + ".kmz", map_output_scale="0", is_composite="NO_COMPOSITE", is_vector_to_raster="VECTOR_TO_IMAGE", extent_to_export="DEFAULT", image_size="1024", dpi_of_client="96", ignore_zvalue="CLAMPED_TO_GROUND")
+        os.remove(kmzDir+"/blocksCurrent.mxd")
+
+print('Done.')
+print('\nStart time: {}'.format(start_time))
+print('End time: {}'.format(date_and_time()))
